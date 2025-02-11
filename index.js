922e577dad47fba6f534049bc77891430dc2eb8f
Gustavo Hortega <gustavohortega80@gmail.com>
2025-02-11 15:54:42 -0400

    configure a view engine e o path para utilizarmos o ejs.

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
+//Import de dependencias
 const express = require('express');
+const ejs = require('ejs');
 require('dotenv').config()
 
-const { connect } = require('./models/index');
+//import do modulo para trabalhar com caminhos
 const path = require('path');
+
+//import do modulo para conexão com o banco
+const { connect } = require('./models/index');
 
 const app = express();
 
+//configuracoes de ejs
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+//configuracoes de json no corpo da requisição
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
@@ -15,6 +25,7 @@ const tasksRouter = require('./routes/api/index');
 //routes
 app.use('/', tasksRouter);
 
+//servidor
 app.listen(process.env.SERVER_PORT, () =>{
     connect();
 
